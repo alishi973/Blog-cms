fafdaef979058584944fed5e5fa85a4b79c91734
ali shirzadeh <shirzadehali973@gmail.com>
2020-05-02 22:21:23 +0430

    Added 'jsonwebtoken'

--- controllers/user.js
+++ controllers/user.js
@@ -2,11 +2,14 @@ const User = require('../models/users');
 const bcrypt = require('bcrypt');
 
 module.exports = {
-  login: ({ email, password }) => {
-    return { token: JSON.stringify(userInput) };
+  login: async ({ email, password }) => {
+    const existUser = await User.findOne({ email, password: bcrypt.hash(password, +process.env.SALT) });
+    console.log(existUser)
+    return {  };
   },
   signUp: async ({ user }) => {
     const existUser = await User.findOne({ email: user.email });
+    //Validation
     if (existUser) throw new Error('User Exist');
     const newUser = new User(user);
     newUser.password = await bcrypt.hash(newUser.password, +process.env.SALT);
